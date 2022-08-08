b39f8647bf617ccc96cfc4e978d6859a4e51f49f
Dany Bechara <bechara611@gmail.com>
2022-08-08 18:19:08 -0400

    f

---
+++ .history/src/elementos/formularioLogin_20220808181855.js
@@ -0,0 +1,27 @@
+import Boton from "./button";
+console.log('hola')
+const FormularioLogin = () => {
+    return ( 
+
+        <>
+        <form action="" className='mt-2 formulario'>
+        <div className='col-12 centrar2 mb-2'><h3>Login</h3></div>
+        <div className="col-12 centrar2">
+        <input type="email" placeholder="EMAIL" className='col-12 mb-2 form-control je' />
+        <input type="password" placeholder="PASSWORD" className='col-12 form-control' />
+        </div>
+         <div className="col-12 centrar2 mt-3">
+         <button className="btn btn-success col-8 mt-2">LOGIN</button>
+         <button className="btn btn-primary col-8 mt-2">REGISTER</button>
+         </div>
+         <div className="centrar2 mt-2 col-12">
+        <Boton className='' >Recovery password</Boton>
+         </div>
+         
+        </form>
+        
+        </>
+     );
+}
+ 
+export default FormularioLogin;
---
+++ .history/src/elementos/quoakka_20220808181659.js
@@ -0,0 +1,5 @@
+console.log('');
+
+const hola='hola'
+
+console.log(hola);
