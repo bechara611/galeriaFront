31a79a289f4e8e463534625cbec35ee1b6bc2468
Dany Bechara <dbechara@MCLCRXVFL>
2022-08-17 09:42:41 -0400

    f

---
+++ .history/src/components/Header2_20220817074824.js
@@ -0,0 +1,26 @@
+import './Header2.css'
+// import {ReactComponent as Heart} from './../imagenes/Heart.svg'
+const HeaderPrincial2 = () => {
+ 
+    return (
+       
+       <header>
+        <nav className="nav-bar navbar-dark  contenedor-header ">
+        <div className="contenedor2 container">
+        <div className="contenedor-titulo">
+        <h5>Gallery App</h5>
+      
+        </div>
+        
+        <div className="contenedor-menu">
+       <i className=" link-red fa-brands fa-linkedin iconosRedes "></i>
+       <i className=" link-red fa-brands fa-linkedin iconosRedes "></i>
+        </div>
+        </div>
+        </nav>
+        </header>
+        
+      );
+}
+ 
+export default HeaderPrincial2;
---
+++ .history/src/components/Header2_20220817074833.js
@@ -0,0 +1,26 @@
+import './Header2.css'
+// import {ReactComponent as Heart} from './../imagenes/Heart.svg'
+const HeaderPrincial2 = () => {
+ 
+    return (
+       
+       <header>
+        <div className="container-fluid contenedor-header ">
+        <div className="contenedor2 container">
+        <div className="contenedor-titulo">
+        <h5>Gallery App</h5>
+      
+        </div>
+        
+        <div className="contenedor-menu">
+       <i className=" link-red fa-brands fa-linkedin iconosRedes "></i>
+       <i className=" link-red fa-brands fa-linkedin iconosRedes "></i>
+        </div>
+        </div>
+        </div>
+        </header>
+        
+      );
+}
+ 
+export default HeaderPrincial2;
