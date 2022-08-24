93413de88a08e2859a58291f32b10fac6b4eaf4f
Dany Bechara <dbechara@MCLCRXVFL>
2022-08-24 15:54:03 -0400

    f

---
+++ .history/src/components/Footer_20220824155316.js
@@ -0,0 +1,14 @@
+import './../components/Footer.css'
+const FooterComponente = () => {
+    return (
+        <>
+        <footer className="container-fluid footer">
+        <div className="col contenedor-footer">
+        <p className="item">Dany Bechara</p>
+        </div>
+        </footer>
+        </>
+      );
+}
+ 
+export default FooterComponente;
